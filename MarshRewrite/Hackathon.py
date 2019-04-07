5b7f3a9cd7446f4da4de66f60165a17940cfa200
Gandavarapu Mounika <mgandava@email.asu.edu>
2019-04-07 11:47:52 -0700

    Integration code
    Have integrated UI, automated email and text feature

--- MarshRewrite/Hackathon.py
+++ MarshRewrite/Hackathon.py
@@ -6,14 +6,28 @@
 
 import numpy as np
 import csv
+import os
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.naive_bayes import GaussianNB
+from collections import defaultdict
+from sklearn import svm
+from sklearn import tree
+import random
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, BaggingClassifier, ExtraTreesClassifier, GradientBoostingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+from sklearn import svm
+from sklearn import tree
+from sklearn.linear_model import LogisticRegression
 
+output = defaultdict(float)
 x = []
 y = []
 
-with open('C:/Hackathon/Code/data.csv') as csvfile:
+fileHome = os.path.expanduser('~')
+fileName = fileHome + r'/Documents/Hackathon/MarshRewrite/data.csv'
+with open(fileName) as csvfile:
 	reader = csv.reader(csvfile, delimiter = ' ')
 	for row in reader:
 		x.append(row[0: (len(row))])
@@ -64,26 +78,18 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, rando
 clfnb = GaussianNB()
 clfnb.fit(x_train, y_train)
 
-print("Naive Bayes classifier")
-print(clfnb.score(x_test, y_test))
-print("\n")
+# print("Naive Bayes classifier")
+# print(clfnb.score(x_test, y_test))
+# print("\n")
+output["Naive Bayes classifier"]=clfnb.score(x_test, y_test)
 
 
 # In[27]:
 
-
-import numpy as np
-import csv
-from sklearn import svm
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
-from sklearn import tree
-import random
-
 x = []
 y = []
 
-with open('C:/Hackathon/Code/data.csv') as csvfile:
+with open(fileName) as csvfile:
 	reader = csv.reader(csvfile, delimiter = ' ')
 	for row in reader:
 		x.append(row[0: (len(row))])
@@ -120,9 +126,10 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.1, rando
 clf = svm.SVC()
 clf.fit(x_train, y_train)
 
-print("SVM rbf kernel Classifier")
-print(clf.score(x_test, y_test))
-print("\n")
+# print("SVM rbf kernel Classifier")
+# print(clf.score(x_test, y_test))
+# print("\n")
+output["SVM rbf kernel Classifier"]=clf.score(x_test, y_test)
 
 #Decision Tree Classifier
 
@@ -131,9 +138,10 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.1, rando
 clftree = tree.DecisionTreeClassifier()
 clftree.fit(x_train, y_train)
 
-print("Decision Tree Classifier")
-print(clftree.score(x_test, y_test))
-print("\n")
+# print("Decision Tree Classifier")
+# print(clftree.score(x_test, y_test))
+# print("\n")
+output["Decision Tree Classifier"]=clftree.score(x_test, y_test)
 
 x = []
 y = []
@@ -160,18 +168,10 @@ print("\n")'''
 
 # In[28]:
 
-
-import numpy as np
-import csv
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, BaggingClassifier, ExtraTreesClassifier, GradientBoostingClassifier
-from sklearn.neighbors import KNeighborsClassifier
-
 x = []
 y = []
 
-with open('C:/Hackathon/Code/data.csv') as csvfile:
+with open(fileName) as csvfile:
 	reader = csv.reader(csvfile, delimiter = ' ')
 	for row in reader:
 		x.append(row[0: (len(row))])
@@ -200,9 +200,10 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.1, rando
 clf = RandomForestClassifier()
 clf.fit(x_train, y_train)
 
-print("Random Forest classifier")
-print(clf.score(x_test, y_test))
-print("\n")
+# print("Random Forest classifier")
+# print(clf.score(x_test, y_test))
+# print("\n")
+output["Random Forest classifier"]=clf.score(x_test, y_test)
 
 #Adaboost Classifier
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.1, random_state = 42)
@@ -210,9 +211,10 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.1, rando
 clf = AdaBoostClassifier()
 clf.fit(x_train, y_train)
 
-print("AdaBoost classifier")
-print(clf.score(x_test, y_test))
-print("\n")
+# print("AdaBoost classifier")
+# print(clf.score(x_test, y_test))
+# print("\n")
+output["AdaBoost classifier"]=clf.score(x_test, y_test)
 
 #BaggingClassifier
 
@@ -221,9 +223,10 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.1, rando
 clf = BaggingClassifier()
 clf.fit(x_train, y_train)
 
-print("Bagging classifier")
-print(clf.score(x_test, y_test))
-print("\n")
+# print("Bagging classifier")
+# print(clf.score(x_test, y_test))
+# print("\n")
+output["Bagging classifier"]=clf.score(x_test, y_test)
 
 #ExtraTreesClassifier
 
@@ -232,9 +235,10 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.1, rando
 clf = ExtraTreesClassifier()
 clf.fit(x_train, y_train)
 
-print("ExtraTrees classifier")
-print(clf.score(x_test, y_test))
-print("\n")
+# print("ExtraTrees classifier")
+# print(clf.score(x_test, y_test))
+# print("\n")
+output["ExtraTrees classifier"]=clf.score(x_test, y_test)
 
 #GradientBoostingClassifier
 
@@ -243,9 +247,10 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.1, rando
 clf = GradientBoostingClassifier()
 clf.fit(x_train, y_train)
 
-print("GradientBoostingClassifier")
-print(clf.score(x_test, y_test))
-print("\n")
+# print("GradientBoostingClassifier")
+# print(clf.score(x_test, y_test))
+# print("\n")
+output["GradientBoostingClassifier"]=clf.score(x_test, y_test)
 
 #Just Something
 
@@ -254,9 +259,10 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.1, rando
 bagging = BaggingClassifier(KNeighborsClassifier(), max_samples=0.5, max_features=0.5)
 bagging.fit(x_train, y_train)
 
-print("Just trying something")
-print(bagging.score(x_test, y_test))
-print("\n")
+# print("Just trying something")
+# print(bagging.score(x_test, y_test))
+# print("\n")
+output["Just trying something for bagging"]=bagging.score(x_test, y_test)
 
 #KneighboursClassifier
 
@@ -265,106 +271,20 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.1, rando
 clf = KNeighborsClassifier()
 clf.fit(x_train, y_train)
 
-print("KNeighborsClassifier")
-print(clf.score(x_test, y_test))
-print("\n")
-
-
-# In[30]:
-
-
-import numpy as np
-import pandas as pd
-import csv
-from sklearn import svm
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
-from sklearn import tree
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import GaussianNB
-
-x = []
-y = []
-
-with open('C:/Hackathon/Code/data.csv') as csvfile:
-	reader = csv.reader(csvfile, delimiter = ' ')
-	for row in reader:
-		x.append(row[0: (len(row))])
-
-for i in x:
-	i[0] = i[0].split(',')
-	y.append(i[0][-1])
-	del i[0][-1]
-
-X = []
-for i in x:
-	X.append(i[0])
-Y = []
-for i in y:
-	Y.append(i)
-
-#print(str(X[0]) + "\n")
-#print(str(X[0])  + "     " + str(Y[4000]) + "\n")
-
-X = np.asarray(X)
-Y = np.asarray(Y)
-
-x = []
-y = []
-
-for i in X:
-	temp = []
-	for j in i:
-		temp.append(float(j))
-	x.append(temp)
-
-for i in Y:
-	temp = []
-	for j in i:
-		temp.append(float(j))
-	y.append(temp)
-
-#print(y[0])
-
-x = np.asarray(x)
-y = np.asarray(y)
-
-#Logistic Regression l1 classifier
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state = 42)
-
-clfl1 = LogisticRegression(penalty = 'l1')
-clfl1.fit(x_train, y_train)
-
-print("Logistic Regression l1 type classifier")
-print(clfl1.score(x_test, y_test))
-print("\n")
-
-#Logistic Regression l2 classifier
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state = 42)
-
-clfl2 = LogisticRegression(penalty = 'l2')
-clfl2.fit(x_train, y_train)
-
-print("Logistic Regression l2 type classifier")
-print(clfl2.score(x_test, y_test))
-print("\n")
+# print("KNeighborsClassifier")
+# print(clf.score(x_test, y_test))
+# print("\n")
+output["KNeighborsClassifier"]=clf.score(x_test, y_test)
 
 
 # In[32]:
 
-
-import numpy as np
-import csv
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import SGDClassifier
 
 x = []
 y = []
 
-with open('C:/Hackathon/Code/data.csv') as csvfile:
+with open(fileName) as csvfile:
 	reader = csv.reader(csvfile, delimiter = ' ')
 	for row in reader:
 		x.append(row[0: (len(row))])
@@ -414,7 +334,115 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, rand
 clf = SGDClassifier()
 clf.fit(x_train, y_train)
 
-print("SGDClassifier")
-print(clf.score(x_test, y_test))
-print("\n")
+# print("SGDClassifier")
+# print(clf.score(x_test, y_test))
+# print("\n")
+output["SGDClassifier"]=clf.score(x_test, y_test)
 
+# In[30]:
+
+x = []
+y = []
+
+with open(fileName) as csvfile:
+	reader = csv.reader(csvfile, delimiter = ' ')
+	for row in reader:
+		x.append(row[0: (len(row))])
+
+for i in x:
+	i[0] = i[0].split(',')
+	y.append(i[0][-1])
+	del i[0][-1]
+
+X = []
+for i in x:
+	X.append(i[0])
+Y = []
+for i in y:
+	Y.append(i)
+
+#print(str(X[0]) + "\n")
+#print(str(X[0])  + "     " + str(Y[4000]) + "\n")
+
+X = np.asarray(X)
+Y = np.asarray(Y)
+
+x = []
+y = []
+
+for i in X:
+	temp = []
+	for j in i:
+		temp.append(float(j))
+	x.append(temp)
+
+for i in Y:
+	temp = []
+	for j in i:
+		temp.append(float(j))
+	y.append(temp)
+
+#print(y[0])
+
+x = np.asarray(x)
+y = np.asarray(y)
+
+#Logistic Regression l1 classifier
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state = 42)
+
+clfl1 = LogisticRegression(penalty = 'l1')
+clfl1.fit(x_train, y_train)
+
+# print("Logistic Regression l1 type classifier")
+# print(clfl1.score(x_test, y_test))
+# print("\n")
+output["Logistic Regression l1 type classifier"]=clfl1.score(x_test, y_test)
+
+#Logistic Regression l2 classifier
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state = 42)
+
+clfl2 = LogisticRegression(penalty = 'l2')
+clfl2.fit(x_train, y_train)
+
+# print("Logistic Regression l2 type classifier")
+# print(clfl2.score(x_test, y_test))
+# print("\n")
+output["Logistic Regression l2 type classifier"]=clfl2.score(x_test, y_test)
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.field_names = ["Classifier", "Score"]
+
+# Everything is collected in outout dictionary
+# Now traverse the dictionary and get the output
+dash = '-' * 40
+result = []
+maxAccuracy = 0
+result.append(["Classifier", "Score"])
+for key, value in output.items():
+	result.append([key,value])
+	table.add_row([key, value])
+	maxAccuracy = max(value,maxAccuracy)
+	#print(key, ' = ',value)
+# for i in range(len(result)):
+#     if i == 0:
+#       print(dash)
+#       print('{:<10s}{:>4s}'.format(result[i][0],result[i][1]))
+#       print(dash)
+#     else:
+#       print('{:<10s}{:>4f}'.format(result[i][0],result[i][1]))
+
+print(table)
+fileOutputName = fileHome + r'/Documents/Hackathon/MarshRewrite/result.csv'
+with open(fileOutputName, 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(result)
+
+if maxAccuracy > 0.6:
+	os.system("python3 automaticEmail.py")
+	os.system("python3 Send_sms.py")
+
+os.system("python3 web.py")
---
+++ MarshRewrite/web.py
@@ -0,0 +1,7 @@
+import webbrowser
+new = 2 # open in a new tab, if possible
+
+
+#open an HTML file on my own (Windows) computer
+url = "file:///Users/mounikachandra/Documents/Hackathon/MarshRewrite/index.html"
+webbrowser.open(url,new=new)
